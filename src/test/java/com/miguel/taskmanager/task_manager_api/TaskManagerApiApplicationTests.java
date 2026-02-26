dd47bec77e6566d9de00d3cd1eca8dffc891dc34
MiguelFloresTabory <miguel.florest.322@gmail.com>
2026-02-26 10:04:57 -0500

    chore: initial commit with complete project implementation

--- src/test/java/com/miguel/taskmanager/task_manager_api/TaskManagerApiApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.miguel.taskmanager.task_manager_api;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TaskManagerApiApplicationTests {
-
-}
